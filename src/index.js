0e84ea3f9ee8fe5ca2a21b25aff27a59d382b4b7
Luminoid <14302010006@fudan.edu.cn>
2018-03-27 00:30:46 +0800

    1. remove ignore files 2. compress images 3. add Document Title

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DocumentTitle from 'react-document-title';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <DocumentTitle title='Color Splash'>
+        <App />
+    </DocumentTitle>,
+    document.getElementById('root')
+);
 registerServiceWorker();
